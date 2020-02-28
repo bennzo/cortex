33ad7a067945d46d6dbc91f1c9c1df5df36138b1
Ben Ohayon <bennohay@gmail.com>
2020-02-28 12:37:41 +0200

    Changed: renamed project

--- cortex/__main__.py
+++ cortex/__main__.py
@@ -1,5 +1,5 @@
 import click
-import magbrain
+import cortex
 import functools
 
 
@@ -21,28 +21,28 @@ def client():
 @server.command()
 @click.option('--address', help='Server address - <host>:<port>')
 @click.option('--data_dir', help='Path of directory containing user thoughts')
-@functools.wraps(magbrain.run_webserver)
+@functools.wraps(cortex.run_webserver)
 def run_webserver(*args, **kwargs):
-    return magbrain.run_webserver(*args, **kwargs)
+    return cortex.run_webserver(*args, **kwargs)
 
 
 @server.command()
 @click.option('--host', help='Server IP address')
 @click.option('--port', help='Server port')
 @click.option('--data', help='Path to the data directory')
-# @functools.wraps(magbrain.run_server)
+# @functools.wraps(cortex.run_server)
 def run(*args, **kwargs):
-    return magbrain.run_server(*args, **kwargs)
+    return cortex.run_server(*args, **kwargs)
 
 
 @client.command()
 @click.option('--host', help='Server IP address')
 @click.option('--port', help='Server port')
 @click.option('--sample', help='Path to the mind sample')
-# @functools.wraps(magbrain.run_client)
+# @functools.wraps(cortex.run_client)
 def run(*args, **kwargs):
-    return magbrain.run_client(*args, **kwargs)
+    return cortex.run_client(*args, **kwargs)
 
 
 if __name__ == '__main__':
-    root(prog_name='magbrain')
+    root(prog_name='cortex')
